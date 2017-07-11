630d548566955ec3e85d8de9ff3b0559ba1cad58
ironkarmafan <ironkarmafan@gmail.com>
2017-07-10 19:16:29 -0500

    added static model for knockout tests

---
+++ js/places.js
@@ -0,0 +1,16 @@
+/*
+Model of places
+TODO: Make this dynamic
+*/
+var initialPlaces = [
+    {
+        name: "Holla Mode",
+        lat: 30.2640942,
+        lng: -97.7658658
+    },
+    {
+        name: "The Science Cream",
+        lat: 30.2484925,
+        lng: -97.7517209
+    }
+]

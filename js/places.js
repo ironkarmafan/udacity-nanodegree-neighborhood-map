d5ce041af3a8ae040e8ca9e448a4571c3b972fc1
ironkarmafan <ironkarmafan@gmail.com>
2017-07-10 21:38:33 -0500

    added cold treat/hot drink bindings

--- js/places.js
+++ js/places.js
@@ -6,16 +6,36 @@ var initialPlaces = [
     {
         name: "Holla Mode",
         lat: 30.2640942,
-        lng: -97.7658658
+        lng: -97.7658658,
+        coldTreats: true,
+        hotDrinks: false
     },
     {
         name: "The Science Cream",
         lat: 30.2484925,
-        lng: -97.7517209
+        lng: -97.7517209,
+        coldTreats: true,
+        hotDrinks: false
     },
     {
         name: "Lick Honest Ice Creams",
         lat: 30.2556041,
-        lng: -97.7648235
+        lng: -97.7648235,
+        coldTreats: true,
+        hotDrinks: false
+    },
+    {
+        name: "The Tasty Spoon",
+        lat: 30.250431,
+        lng: -97.7567444,
+        coldTreats: true,
+        hotDrinks: true
+    },
+    {
+        name: "Summer Moon Coffee",
+        lat: 30.2332686,
+        lng: -97.7670597,
+        coldTreats: false,
+        hotDrinks: true
     }
 ]
--- js/vm.js
+++ js/vm.js
@@ -2,6 +2,8 @@ var Place = function(d) {
     this.name = d.name;
     this.lat = d.lat;
     this.lng = d.lng;
+    this.coldTreats = d.coldTreats;
+    this.hotDrinks = d.hotDrinks;
 }
 /*
 for(var i=0; i<initialPlaces.length; i++) {
@@ -35,6 +37,30 @@ function viewModel() {
             m.setMap(map);
             m.animation = google.maps.Animation.DROP;
         });
+    }
+
+    this.showHotDrinks = function() {
+        for(var i=0; i<initialPlaces.length; i++){
+            if(initialPlaces[i].hotDrinks == true) {
+                markers[i].setMap(map);
+                markers[i].animation = google.maps.Animation.DROP;
+            }
+            else {
+                markers[i].setMap(null);
+            }
+        }
+    }
+
+    this.showColdTreats = function() {
+        for(var i=0; i<initialPlaces.length; i++){
+            if(initialPlaces[i].coldTreats == true) {
+                markers[i].setMap(map);
+                markers[i].animation = google.maps.Animation.DROP;
+            }
+            else {
+                markers[i].setMap(null);
+            }
+        }
     }
 }
 
